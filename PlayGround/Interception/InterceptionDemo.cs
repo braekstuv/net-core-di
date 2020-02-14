0e1af54b708fffdcd53b4783c9c1e919816d4afd
Florian Pirard <Florian.Pirard@realdolmen.com>
2020-02-14 21:01:24 +0100

    added interception demo

---
+++ PlayGround/Interception/InterceptionDemo.cs
@@ -0,0 +1,59 @@
+using System;
+using System.IO;
+using System.Linq;
+using Castle.DynamicProxy;
+
+namespace PlayGround.Interception
+{
+    public class InterceptionDemo
+    {
+        public static void Run()
+        {
+            ICalculator calculator = new Calculator();
+            var proxy = new ProxyGenerator().CreateInterfaceProxyWithTarget(calculator, new MyLogger(Console.Out));
+            proxy.Divide(5, 5);
+            proxy.Divide(5, 0);
+        }
+
+
+    }
+
+    public class Calculator : ICalculator
+    {
+        public double Divide(int x, int y)
+        {
+            return x / y;
+        }
+    }
+    public class MyLogger : IInterceptor
+    {
+        private readonly TextWriter _output;
+        public MyLogger(TextWriter output)
+        {
+            _output = output;
+        }
+        public void Intercept(IInvocation invocation)
+        {
+            try
+            {
+                _output.WriteLine("Calling {0}.{1} with parameters {2}... ",
+                    invocation.TargetType.Name,
+                    invocation.Method.Name,
+                    string.Join(", ", invocation.Arguments.Select(a => (a ?? "").ToString()).ToArray()));
+                invocation.Proceed();
+
+                _output.WriteLine("Done: result was {0}.", invocation.ReturnValue);
+            }
+            catch (Exception)
+            {
+                _output.WriteLine("Exception thrown during call.");
+                throw;
+            }
+        }
+    }
+
+    public interface ICalculator
+    {
+        double Divide(int x, int y);
+    }
+}
--- PlayGround/Program.cs
+++ PlayGround/Program.cs
@@ -2,6 +2,8 @@
 using System.Linq;
 using Castle.DynamicProxy;
 using PlayGround.CustomServiceCollections;
+using PlayGround.Interception;
+
 namespace PlayGround
 {
     class Program
@@ -9,27 +11,8 @@ namespace PlayGround
         static void Main(string[] args)
         {
             //ScopesDemo.Run();
-            CustomServiceCollectionDemo.Run();
-        }
-    }
-
-
-    public class MyLogger : IInterceptor
-    {
-        private readonly TextWriter _output;
-        public MyLogger(TextWriter output)
-        {
-            _output = output;
-        }
-        public void Intercept(IInvocation invocation)
-        {
-            _output.Write("Calling method {0} with parameters {1}... ",
-            invocation.Method.Name,
-            string.Join(", ", invocation.Arguments.Select(a => (a ?? "").ToString()).ToArray()));
-
-            invocation.Proceed();
-
-            _output.WriteLine("Done: result was {0}.", invocation.ReturnValue);
+            //CustomServiceCollectionDemo.Run();
+            InterceptionDemo.Run();
         }
     }
 }
