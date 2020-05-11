de9d51b488623790f8e9884f704b60c3d3e85029
Florian Pirard <Florian.Pirard@realdolmen.com>
2020-05-11 23:41:09 +0200

    fix for demo

--- DiDemo.Console/Program.cs
+++ DiDemo.Console/Program.cs
@@ -5,28 +5,25 @@ using DiDemo.Common.Services;
 using DiDemo.Common.Services.DemoDependencies;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace DiDemo.CaptiveDependenciesDemo.ConsoleApp
-{
-    class Program
-    {
-        static async Task Main(string[] args)
-        {
+namespace DiDemo.CaptiveDependenciesDemo.ConsoleApp {
+    class Program {
+        static async Task Main(string[] args) {
             var serviceCollection = new ServiceCollection();
-            
+
             //Underlying dependencies will be disposed when the scope is disposed.
             serviceCollection.AddScoped<MyCoolService>()
-             .AddScoped<MyOtherCoolService>();
+                .AddScoped<MyOtherCoolService>();
 
             //Underlying dependencies will be disposed when the serviceprovider is disposed.
             // serviceCollection.AddSingleton<MyCoolService>()
             // .AddSingleton<MyOtherCoolService>();
 
-            serviceCollection.AddSingleton<ISingletonDependency, SingletonDependency>()
-            .AddScoped<IScopedDependency, ScopedDependency>()
-            .AddTransient<ITransientDependency, TransientDependency>();
+            serviceCollection.AddSingleton<SingletonDependency>()
+                .AddScoped<ScopedDependency>()
+                .AddTransient<TransientDependency>();
 
             //Line below will not throw an error
-            using (var serviceProvider = serviceCollection.BuildServiceProvider())
+            using(var serviceProvider = serviceCollection.BuildServiceProvider())
             //Line below will throw an error if there are captive dependencies
             // using (var serviceProvider = serviceCollection.BuildServiceProvider(true))
             //Line below will throw an error
@@ -38,7 +35,7 @@ namespace DiDemo.CaptiveDependenciesDemo.ConsoleApp
                 var task1 = RunScope(serviceProvider);
                 Thread.Sleep(10);
                 var task2 = RunScope(serviceProvider);
-                var tasks = new[]{
+                var tasks = new [] {
                     task1,
                     task2
                 };
@@ -47,10 +44,8 @@ namespace DiDemo.CaptiveDependenciesDemo.ConsoleApp
             };
         }
 
-        private static Task RunScope(ServiceProvider serviceProvider)
-        {
-            using (var scope = serviceProvider.CreateScope())
-            {
+        private static Task RunScope(ServiceProvider serviceProvider) {
+            using(var scope = serviceProvider.CreateScope()) {
                 var myCoolService = scope.ServiceProvider.GetService<MyCoolService>();
                 Console.WriteLine(string.Join('\n', myCoolService.GetDescriptions()));
                 System.Console.WriteLine("Disposing scope");
